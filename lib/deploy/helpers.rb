b306f6b2910e84e1b3c8979f22195eecdf7db0aa
Derek Kastner <dkastner@gmail.com>
2013-05-21 21:09:13 -0500

    import from stats-chef

---
+++ lib/deploy/helpers.rb
@@ -0,0 +1,43 @@
+module Deploy
+  module Helpers
+    def report(txt, done = 'done!')
+      print txt
+      STDOUT.flush
+      yield
+      puts done
+    end
+
+    def dot
+      print '.'
+      STDOUT.flush
+    end
+
+    def sh(cmd, fail = true)
+      if execution_mode == :popen
+        out = ''
+        IO.popen(cmd) do |io|
+          data = io.gets
+          puts data
+          out += data.to_s
+        end
+        out
+      else
+        exec cmd
+      end
+    end
+
+    def wait(host)
+      report "Waiting for server #{host}", 'ready!' do
+        waiting = true
+        while waiting
+          status = ''
+          IO.popen(%{ssh -y -i ~/.ssh/inspire-www.pem -o "ConnectTimeout=5" -o "StrictHostKeyChecking=false" ubuntu@#{host} "echo OK"}) do |ssh|
+            status += ssh.gets.to_s
+          end
+          waiting = (status !~ /OK/)
+          dot
+        end
+      end
+    end
+  end
+end
