1a2149e3413e73822164dbedb90fcc01b2ce5eb0
Derek Kastner <dkastner@gmail.com>
2013-08-16 13:34:52 -0500

    Simplify importer task

--- config/deploy_goodberry_importer.rb
+++ config/deploy_goodberry_importer.rb
@@ -1,41 +1,4 @@
-require 'net/http'
-require "yaml"
-require 'json'
-
-set :default_environment, {
-  'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
-}
-
-before :production, :default_config
-before :hou, :default_config
-
-set :use_sudo, false
-
-## ssh options
-ssh_options[:forward_agent] = true
-default_run_options[:pty] = true
-
-hosts = JSON.parse(File.read('config/hosts.json'))
-
-hosts.each do |site, type_data|
-  task site do
-    set :site, site
-  end
-
-  type_data.each do |type, color_data|
-    task type do
-      set :type, type
-    end
-
-    color_data.each do |color, host_data|
-      task color do
-        set :color, color
-      end
-    end
-  end
-end
-
-task :config do
+task :deploy_app_importer do
   host = hosts[site][type][color]['ip']
 
   set :user, "app"
@@ -46,13 +9,10 @@ task :config do
     'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
   }
 
-  set :rails_env, "production"
   role :importer, host
-end
 
-root = "/home/app/importer"
+  root = "/home/app/importer"
 
-task :deploy_app_importer do
   bundle_flags = "--deployment --quiet --binstubs"
   run "cd #{root} && bundle install #{bundle_flags}"
   run "cd #{root} && rake import"
